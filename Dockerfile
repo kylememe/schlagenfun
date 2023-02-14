8d6edb02001a4893f7021c436c244ae30b2939fb
ksmith@nethealth.com <ksmith@nethealth.com>
2023-02-13 19:09:34 -0500

    Got things working on user defined bridge network

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
 FROM python:3.8-slim
 
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc    
+
 EXPOSE 5000
 
 # Keeps Python from generating .pyc files in the container
--- launchpostgrescontainer.sh
+++ launchpostgrescontainer.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --name postgresql -e POSTGRES_USER=schlagenuser -e POSTGRES_PASSWORD= -p 5432:5432 -v /Users/kylesmith/Repos/schlagenfun/instance/postgres:/var/lib/postgresql/data -d postgres
+docker run \
+--name db \
+--env POSTGRES_USER=schlagenuser \
+--env POSTGRES_PASSWORD= \
+--volume /Users/kylesmith/Repos/schlagenfun/instance/postgres:/var/lib/postgresql/data \
+--network=schlagen-net \
+--detach \
+postgres
